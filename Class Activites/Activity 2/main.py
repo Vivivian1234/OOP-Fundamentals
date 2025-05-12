95b113f374fcbef7489c9944032bbf16de73e77d
Vivivian1234 <vivian.ding1@education.nsw.gov.au>
2025-05-12 15:08:19 +1000

    hmm
    send help

--- Class Activites/Activity 2/main.py
+++ Class Activites/Activity 2/main.py
@@ -1,4 +1,19 @@
-from subjects import Subject
+from subjects import Subject, List
+
+action = int(input("What would you like to do? \n- 1. Add subject \n- 2. View a chosen subject from the list \n- 3. View all subjects\n- 4.Exit \n"))
+
+while action != 4:
+    if action == 1:
+        List.add_subject()
+    elif action == 2:
+        List.view_subject()
+    elif action == 3:
+        List.print_all_subjects()
+    else:
+        print("Input not valid. Please try again.\n")
+        action = int(input("What would you like to do? \n- 1. Add subject \n- 2. View a chosen subject from the list \n- 3. View all subjects\n- 4.Exit \n"))
+
+print ("\nThank you for using this program.")
 
 
-print(Subject)
+
--- Class Activites/Activity 2/subjects.py
+++ Class Activites/Activity 2/subjects.py
@@ -1,5 +1,3 @@
-list = []
-
 class Subject:
     def __init__(self, name, year_level, class_code, num_students):
         self.name = name
@@ -16,44 +14,50 @@ class List:
         year_level = input("Enter year level: ")
         class_code = input("Enter class code: ")
         num_students = int(input("Enter number of students in the class: "))
-        
+
         self.list.append(Subject(name, year_level, class_code, num_students))
+        print("\nSubject added.")
+        return
+    
+    def view_subjects(self):
+        subject_name = input("What subject would you like to view? ")
 
-        print ("\nSubject added.")
+        if not self.list:
+            print("\nNo subjects have been added.")
+            return
+        
+        elif subject_name not in self.list:
+            print ("Subject not found. Please try again.")
+            return
 
-    def view_subjects():
-        subject = input("")
 
+        for subject in self.list:
+            if subject.name == subject_name:
+                print(f"\nSubject: {subject.name}\nYear Level: {subject.year_level}\nClass Code: {subject.class_code}\nNumber of Students: {subject.num_students}")
+                return
 
+    def print_all_subjects(self):
+        if not self.list:
+            print("\nNo subjects have been added.")
+            return
+        
+        print("\nAll Subjects:")
+        for subject in self.list:
+            print(f"\nSubject: {subject.name}\nYear Level: {subject.year_level}\nClass Code: {subject.class_code}\nNumber of Students: {subject.num_students}")
 
-
-
-
-info = Subject(name, year_level, class_code, num_students)
-
-print(f'\nStudent name: {info.name()}')
-print(f'Year Level: {info.year_level()}')
-print(f'Class code: {info.class_code()}')
-print(f'Number of students: {info.num_students}')
+subjects_list = List()
 
 action = int(input("What would you like to do? \n- 1. Add subject \n- 2. View a chosen subject from the list \n- 3. View all subjects\n- 4.Exit \n"))
 
-while action != 4:
-    if action = 1:
-        list.add_subject()
-    elif action = 2:
-        list.view_subject()
-
-
-
-
-
-
-
-
-
-
-
-add_subject_name = input("Add subject: ")
-view_chosen_subject = input("View a chosen subject from the list: ")
-vie_all_subjects = input("View all subjects: ")
+while True:
+    if action == 1:
+        subjects_list.add_subject()
+    elif action == 2:
+        subjects_list.view_subjects()
+    elif action == 3:
+        subjects_list.print_all_subjects()
+    elif action == 4:
+        print("\nThank you for using this program.\n")
+        break
+    else:
+        print("Invalid input. Please try again.")
