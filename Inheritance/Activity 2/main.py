8669de502634d3bf3d356f4b78b92f90f0d33036
Vivivian1234 <vivian.ding1@education.nsw.gov.au>
2025-05-28 10:09:41 +1000

    jythgertw
    qatyeuyikjujy

--- Inheritance/Activity 2/main.py
+++ Inheritance/Activity 2/main.py
@@ -2,26 +2,36 @@ from weapons import Weapon, Sword, Bow, Longbow, Shortbow
 
 weapon_list = Weapon
 
-# Creating instances
-basic_sword = Sword("Excalibur", 50, "Slashing")
-basic_bow = Bow("Hunter's Bow", 30, "Piercing")
-longbow = Longbow("Eagle Longbow", 40, "Piercing", 150)
-shortbow = Shortbow("Swift Shortbow", 35, "Piercing", 80)
 
 def main():
     while True:
-        action = input("This is the weapon recording system. What would you like to do? \n- 1. Add weapon \n- 2. View inventory \n- 3. Search inventory \n- 4. Exit")
+        action = input("This is the weapon recording system. What would you like to do? \n- 1. Add weapon \n- 2. View inventory \n- 3. Search inventory \n- 4. Exit\n")
 
-        if action == 1:
-            weapon_list.add_weapon()
-        elif action == 2:
+        if action == "1":
+            weapon = input("What weapon woudl you like to add?\n- 1. Sword \n- 2. Bow \n- 3. Longbow \n- 4. Shortbow\n")
+            if weapon == "1":
+                damage = input("Enter damage: ")
+                weapon_list.get_stats(Sword)
+            elif weapon == "2":
+                damage = input("Enter damage: ")
+                weapon_list.get_stats(Bow)
+            elif weapon == "3":
+                damage = input("Enter damage: ")
+                weapon_list.get_stats(Longbow)
+            elif weapon == "4":
+                damage = input("Enter damage: ")
+                weapon_list.get_stats(Shortbow)
+            else:
+                print("\nInvalid input. Please try again.\n")
+        elif action == "2":
             weapon_list.view_inventory()
-        elif action == 3:
+        elif action == "3":
             weapon_list.search_inventory()
-        elif action == 4:
+        elif action == "4":
             print("\nThank you for using this program.\n")
             break
         else:
             print("\nInvalid input. Please try again.\n")
 
+main()
 
--- Inheritance/Activity 2/weapons.py
+++ Inheritance/Activity 2/weapons.py
@@ -17,3 +17,35 @@ class Sword(Weapon):
     def get_stats(self):
         crit_damage = self.damage * 3
         print(f"Name: {self.name}, Category: {self.category}, Damage: {self.damage}, Damage Type: {self.damage_category}, Crit Damage: {crit_damage}")
+
+
+class Bow(Weapon):
+    def __init__(self, name, damage, bow_range):
+        super().__init__(name, "Bow", damage)
+        bow_range = "Piercing"
+    
+    def get_stats(self):
+        crit_damage = self.damage * 2
+        print(f"Name: {self.name}, Category: {self.category}, Damage: {self.damage}, Damage Type: {self.bow_range}, Crit Damage: {crit_damage}")
+
+
+class Longbow(Weapon):
+    def __init__(self, name, damage, damage_category):
+        super().__init__(name, "Longbow", damage)
+        self.damage_category = damage_category
+    
+    def get_stats(self):
+        crit_damage = self.damage * 2
+        print(f"Name: {self.name}, Category: {self.category}, Damage: {self.damage}, Damage Type: {self.damage_category}, Crit Damage: {crit_damage}")
+
+class Shortbow(Weapon):
+    def __init__(self, name, damage, damage_category):
+        super().__init__(name, "Shortbow", damage)
+        self.damage_category = damage_category
+    
+    def get_stats(self):
+        crit_damage = self.damage * 2
+        print(f"Name: {self.name}, Category: {self.category}, Damage: {self.damage}, Damage Type: {self.damage_category}, Crit Damage: {crit_damage}")
+
+
+
